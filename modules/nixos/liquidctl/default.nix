b201355d258f998505be3ed46a12a87157726bcd
Nicolas Cura <nicolas.cura@icloud.com>
2025-06-28 13:43:28 +0200

    Modify liquidctl fan speeds

--- modules/nixos/liquidctl/default.nix
+++ modules/nixos/liquidctl/default.nix
@@ -8,8 +8,10 @@
       ExecStart = [
         "${pkgs.liquidctl}/bin/liquidctl initialize all"
         "${pkgs.liquidctl}/bin/liquidctl --match aura set sync color static ff5a2f"
-        "${pkgs.liquidctl}/bin/liquidctl --match ST set fans speed 20 0  60 25  65 75  70 100"
-        "${pkgs.liquidctl}/bin/liquidctl --match XT set fans speed 20 0  60 25  65 75  70 100"
+        # "${pkgs.liquidctl}/bin/liquidctl --match ST set fans speed 20 0  60 25  65 75  70 100"
+        # "${pkgs.liquidctl}/bin/liquidctl --match XT set fans speed 20 50  60 50  65 75  70 100"
+        "${pkgs.liquidctl}/bin/liquidctl --match ST set fans speed 30"
+        "${pkgs.liquidctl}/bin/liquidctl --match XT set fans speed 30"
       ];
     };
   };
