6668fb6062458ce4f970e77da4370e248876f23b
Nicolas Cura <nicolas.cura@icloud.com>
2025-10-22 08:21:38 +0200

    feat(neovim): configure Rust compilation for fff.nvim
    Add native build configuration for fff.nvim plugin with:
    - Rust nightly toolchain from rust-overlay
    - Cargo vendor dependencies with proper hash
    - Build phase for release compilation
    - Required native dependencies (perl, pkg-config, openssl)
    
    This enables proper compilation of the plugin's Rust backend.

--- modules/home-manager/shared/neovim-nix/plugin-packages/default.nix
+++ modules/home-manager/shared/neovim-nix/plugin-packages/default.nix
@@ -1,4 +1,12 @@
 { pkgs }:
+let
+  fff-src = pkgs.fetchFromGitHub {
+    owner = "dmtrKovalenko";
+    repo = "fff.nvim";
+    rev = "e8850c3c62a13e92f71233350962e842fcabf01b";
+    sha256 = "sha256-qyzM45FaXqLipnBW2zTao2SvY21qiFsdsX+Mn2Tu3xI=";
+  };
+in
 {
   # Custom plugins not available or outdated in nixpkgs
   # Each plugin should be built using pkgs.vimUtils.buildVimPlugin
@@ -6,12 +14,30 @@
   fff-nvim = pkgs.vimUtils.buildVimPlugin {
     pname = "fff.nvim";
     version = "2024-10-21";
-    src = pkgs.fetchFromGitHub {
-      owner = "dmtrKovalenko";
-      repo = "fff.nvim";
-      rev = "e8850c3c62a13e92f71233350962e842fcabf01b";
-      sha256 = pkgs.lib.fakeSha256; # Will need to update after first build
+    src = fff-src;
+
+    nativeBuildInputs = with pkgs; [
+      rustPlatform.cargoSetupHook
+      rust-bin.nightly.latest.default
+      perl
+      pkg-config
+      openssl
+    ];
+
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      src = fff-src;
+      name = "fff.nvim-vendor";
+      hash = "sha256-ZZt4BlMgRik4LH92F5cgS84WI1Jeuw68jP+y1+QXfDE=";
     };
+
+    buildPhase = ''
+      runHook preBuild
+      cargo build --release
+      runHook postBuild
+    '';
+
+    # Skip neovim require check since it needs runtime cargo build
+    doCheck = false;
   };
 
   # Add more custom plugins here as needed
